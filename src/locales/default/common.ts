15d8e6781ab01c30f076187530fa4129d4b588c6
canisminor1990 <i@canisminor.cc>
2025-11-19 22:35:10 +0800

    style: update sidebar

--- src/locales/default/common.ts
+++ src/locales/default/common.ts
@@ -286,6 +286,10 @@ export default {
     business: '商务合作',
     support: '邮件支持',
   },
+  navPanel: {
+    agent: '助手',
+    repo: '仓库',
+  },
   oauth: 'SSO 登录',
   officialSite: '官方网站',
   ok: '确定',
